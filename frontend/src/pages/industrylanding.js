b345ffc93a71bc035ed4ca145428f319f4be8dff
jacobposton6 <jacob.poston.6@gmail.com>
2020-03-04 15:11:43 -0600

    Created wireframe for homepage and stock landing page
    Co-authored-by: Jacob <jacob.poston.6@gmail.com>
    Co-authored-by: Kunal <kunaljain@utexas.edu>

---
+++ frontend/src/pages/industrylanding.js
@@ -0,0 +1,65 @@
+import React from "react"
+import Helmet from "react-helmet"
+
+const Industrylanding = () => (
+  <>
+    <Helmet>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"></link>
+    </Helmet>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Top 500 Stocks</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">News</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Company Info</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+            </li>
+        </ul>
+    </nav>
+    <div>
+	    <h2 class="home" style={{marginTop:15, marginLeft:15}}>Industry Performance</h2>
+    </div>
+    <br></br>
+    <table class="table table-dark">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
+    </>
+)
+
+export default Industrylanding
