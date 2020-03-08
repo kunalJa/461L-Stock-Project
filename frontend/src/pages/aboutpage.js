e3b0030ef55a941983b50ba88bcaf2d824632c55
Bala Bala <balakumaran@gmail.com>
2020-03-08 03:45:05 -0500

    fix about page

--- frontend/src/pages/aboutpage.js
+++ frontend/src/pages/aboutpage.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react"
 import Navbar from "../components/Navbar"
 
 const Aboutpage = () => {
-  const [respy, setResp] = useState({});
+  const [respy, setResp] = useState({meme: {}});
   useEffect(() => {
     const xhr2 = new XMLHttpRequest();
     xhr2.addEventListener('load', function () {
@@ -13,8 +13,16 @@ const Aboutpage = () => {
         const xhr = new XMLHttpRequest();
         xhr.addEventListener('load', function () {
           const resp = JSON.parse(this.responseText);
-          respy[email] = resp;
-          setResp(respy);
+          respy.meme = {
+            ...respy.meme,
+            [email]: resp,
+          };
+          setResp({
+            meme: {
+              ...respy.meme,
+              [email]: resp,
+            }
+          });
         });
         xhr.open('POST', 'https://api.github.com/graphql');
         xhr.setRequestHeader('Authorization', 'bearer 9dec6f3f6c84fa4dc8b400b8854a54d06a5d9e8c');
@@ -68,22 +76,75 @@ const Aboutpage = () => {
           }
         }`
     });
-    console.log(t)
     xhr2.send(t);
   }, []);
 
   let happy = null;
-  console.log('hi');
-  console.log(Object.getOwnPropertyNames(respy).length);
-  if (Object.getOwnPropertyNames(respy).length) {
+  console.log(respy);
+  if (Object.keys(respy.meme).length) {
     happy = {};
-    for (const key in Object.getOwnPropertyNames(respy)) {
-      happy[key] = respy[key].data.repository.object.history.totalCount
+    for (const key of Object.keys(respy.meme)) {
+      happy[key] = respy.meme[key].data.repository.object.history.totalCount
     }
   }
-  console.log(happy);
 
-
+  const people = [
+    {
+      "name": "Kunal Jain",
+      "bio": `
+        Bio: I'm pretty much the smart one.
+        Major/Track: ECE/Software Engineering
+        Primary responsibilites: Backend development, database management, API management
+      `,
+      "commitCount": happy ? happy['kunaljain@utexas.edu'] : ''
+    },
+    {
+      "name": "Yulei Xu",
+      "bio": `
+        Bio: Be happy with who you are. Even if no one else likes you, make sure you still like you.
+        Major/Track: ECE/Software Engineering
+        Primary responsibilites: Backend development, database management, API management
+      `,
+      "commitCount": ''
+    },
+    {
+      "name": "Justin Liu",
+      "bio": `
+        Bio: I'm just here doing things and stuff you know? Like stuff kind of things.
+        Major/Track: ECE/Software Engineering
+        Primary responsibilites: Writing, CSS styling, frontend web design
+      `,
+      "commitCount": ''
+    },
+    {
+      "name": "Naveen Yarlagadda",
+      "bio": `
+        Bio: My life goal is to one day have a mustache as glorious as my father's
+        Major/Track: ECE/Software Engineering
+        Primary responsibilites: Backend development, database management, API management
+      `,
+      "commitCount": ''
+    },
+    {
+      "name": "Balakumaran Balasubramanian",
+      "bio": `
+        Bio: Web development sure is.....it sure is.
+        Major/Track: ECE/Software Engineering
+        Primary responsibilites: Backend development, database management, API management
+      `,
+      "commitCount": happy ? happy['balakumaran55@gmail.com'] : ''
+    },
+    {
+      "name": "Jacob Poston",
+      "bio": `
+        Bio: I am an honest man
+        Major/Track: ECE/Software Engineering
+        Primary responsibilites: Writing, CSS Styling, Frontend Design, Moral Support
+      `,
+      "commitCount": happy ? happy['jacob.poston.6@gmail.com'] : ''
+    }
+  ];
+  
   return (
     <>
       <Navbar />
@@ -94,104 +155,35 @@ const Aboutpage = () => {
       </div>
       <br />
       <div className="card-group" style={{ padding: 15 }}>
-        <div className="card">
-          <img className="card-img-top" src="..." alt="Card image cap"></img>
-          <div className="card-body">
-            <h5 className="card-title">Kunal Jain</h5>
-            <p className="card-text">
-              Bio: I'm pretty much the smart one.<br />
-              Major/Track: ECE/Software Engineering<br />
-              Primary responsibilites: Backend development, database management, API management<br />
-            </p>
-            <p>Commits: {happy ? happy['kunaljain@utexas.edu'] : ''}</p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </div>
-        </div>
-        <div className="card">
-          <img className="card-img-top" src="..." alt="Card image cap"></img>
-          <div className="card-body">
-            <h5 className="card-title">Yulei Xu</h5>
-            <p className="card-text">
-              Bio: Be happy with who you are. Even if no one else likes you, make sure you still like you.<br />
-              Major/Track: ECE/Software Engineering<br />
-              Primary responsibilites: Editor, CSS styling, frontend web design.<br />
-            </p>
-          </div>
-        </div>
-        <div className="card">
-          <img className="card-img-top" src="..." alt="Card image cap"></img>
-          <div className="card-body">
-            <h5 className="card-title">Justin Liu</h5>
-            <p className="card-text">
-              Bio: I'm just here doing things and stuff you know? Like stuff kind of things.<br />
-              Major/Track: ECE/Software Engineering. <br />
-              Primary responsibilites: Writing, CSS styling, frontend web design<br />
-            </p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </div>
-        </div>
-      </div>
-      <div className="card-group" style={{ padding: 15 }}>
-        <div className="card">
-          <img className="card-img-top" src="..." alt="Card image cap"></img>
-          <div className="card-body">
-            <h5 className="card-title">Naveen Yarlagadda</h5>
-            <p className="card-text">
-              Bio: My life goal is to one day have a mustache as glorious as my father's<br />
-              Major/Track: ECE/Software Engineering<br />
-              Primary responsibilites: Backend web design, database management<br />
-            </p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </div>
-        </div>
-        <div className="card">
-          <img className="card-img-top" src="..." alt="Card image cap"></img>
-          <div className="card-body">
-            <h5 className="card-title">Bala Balasubramanian</h5>
-            <p className="card-text">
-              Bio: Web development sure is.....it sure is.<br />
-              Major/Track: ECE/Software Engineering<br />
-              Primary responsibilites: Editor, backend web design<br />
-            </p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </div>
-        </div>
-        <div className="card">
-          <img className="card-img-top" src="..." alt="Card image cap"></img>
-          <div className="card-body">
-            <h5 className="card-title">Jacob Poston</h5>
-            <p className="card-text">
-              Bio: Just a kid that likes to make videos<br />
-              Major/Track: ECE/Software Engineering<br />
-              Primary responsibilites: Frontend structure design<br />
-            </p>
-          </div>
-        </div>
-        <div className="card">
-          <img className="card-img-top" src="..." alt="Card image cap"></img>
-          <div className="card-body">
-            <h5 className="card-title">Jacob Poston</h5>
-            <p className="card-text">
-              This is a wider card with supporting text below as a natural lead-in
-              to additional content. This card has even longer content than the
-              first to show that equal height action.
-            </p>
-          </div>
-          <div className="card-footer">
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </div>
-        </div>
+        {/* {[
+          <PersonCard
+            name="Kunal Jain"
+            bio="
+              Bio: I'm pretty much the smart one.
+              Major/Track: ECE/Software Engineering
+              Primary responsibilites: Backend development, database management, API management
+            "
+            commitCount={happy ? happy['kunaljain@utexas.edu'] : ''}
+          />
+        ]} */}
+        {people.map(person => <PersonCard name={person.name} bio={person.bio} commitCount={person.commitCount} key={person.name} />)}
       </div>
     </>
   );
 }
+
+const PersonCard = ({name, bio, commitCount}) => (
+  <div className="card">
+    <img className="card-img-top" src="..." alt="Card image cap"></img>
+    <div className="card-body">
+      <h5 className="card-title">{name}</h5>
+      <p className="card-text">{bio}</p>
+      <p>Commits: {commitCount}</p>
+    </div>
+    <div className="card-footer">
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </div>
+  </div>
+);
 
 export default Aboutpage
