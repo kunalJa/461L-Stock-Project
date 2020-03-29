a75130a3ac7d7f29538200a4770c030a1e646092
kunalja <kunaljain@utexas.edu>
2020-03-28 21:01:52 -0500

    Fix error where code was deleted in merge

--- frontend/src/pages/industrylanding.js
+++ frontend/src/pages/industrylanding.js
@@ -1,46 +1,56 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
+
 import Navbar from "../components/Navbar"
 
-const Industrylanding = () => (
-  <>
-    <Navbar />
-    <div>
-      <h2 class="home" style={{ marginTop: 15, marginLeft: 15 }}>
-        Industry Performance
-      </h2>
-    </div>
-    <br></br>
-    <table class="table table-dark">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Larry</td>
-          <td>the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </table>
-  </>
-)
+const Industrylanding = ({ data }) => {
+  const industries = data.allMongodbStockInformationIndustry.edges
+  return (
+    <>
+      <Navbar />
+      <div>
+        <h2 class="home" style={{ marginTop: 15, marginLeft: 15 }}>
+          All Industries: Click on an industry for more info!
+        </h2>
+      </div>
+      <br></br>
+      <table class="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+          </tr>
+        </thead>
+        {industries.map(({ node }, i) => {
+          return (
+            <tr key={i}>
+              <td>
+                <Link to={`/industry/${node.name.split(" ").join("")}`}>
+                  {node.name}
+                </Link>
+              </td>
+            </tr>
+          )
+        })}
+      </table>
+      <img
+        style={{ width: "800px" }}
+        src={"https://miro.medium.com/max/1000/1*yPElCHCwklSJez2yKEh3Cw.jpeg"}
+        alt=""
+      />
+    </>
+  )
+}
 
 export default Industrylanding
+
+export const industryData = graphql`
+  query allIndustry {
+    allMongodbStockInformationIndustry {
+      edges {
+        node {
+          name
+        }
+      }
+    }
+  }
+`
