4824adaeaaf7699d44ae5edbab553f11d2e04dac
Yulei Xu <yuleixu@utexas.edu>
2020-03-29 20:53:25 -0500

    Added chart for past 7 days of data

--- frontend/src/layouts/StockPage.jsx
+++ frontend/src/layouts/StockPage.jsx
@@ -10,9 +10,13 @@ import Card from "../components/Card"
 export default function StockPage({ data }) {
   const stock = data.mongodbStockInformationInformation
   const historicData = {}
+  const weeksData = {}
   const csvData = []
   stock.historical[0].dates.forEach((key, i) => {
     historicData[key] = stock.historical[0].prices[i]
+    if (i >= stock.historical[0].dates.length - 7) {
+      weeksData[key] = stock.historical[0].prices[i]
+    }
     csvData.push({
       symbol: stock.symbol,
       date: key,
@@ -29,14 +33,16 @@ export default function StockPage({ data }) {
       <h4 style={{ marginLeft: 45 }}>Volume: {stock.latestVolume}</h4>
       <h4 style={{ marginLeft: 45 }}>Industry: {stock.industry}</h4>
       <br></br>
-
-      <LineChart data={historicData} />
+      
+      <h2 style={{ marginLeft: 45, fontWeight: 'bold'}}>Historical Data</h2>
       <ExportCSV
         csvData={csvData}
         fileName={`${stock.symbol}-data`}
         buttonText={`Download ${stock.name}'s historical data as a CSV file`}
       />
-
+      <LineChart data={historicData} min = {null} curve = {false}/>
+      <h2 style={{ marginLeft: 45, fontWeight: 'bold'}}>Recent Changes (2020)</h2>
+      <LineChart data= {weeksData} min = {null} curve = {false}/>
       <h2 style={{ marginLeft: 45, fontWeight: 'bold'}}>Recent News:</h2>
       <div className="card-deck" style={{ paddingLeft: 45, paddingRight: 45 }}>
         {stock.news.data.slice(1, 3).map((article, i) => {
