855469f694401e2d4f18d1e8efeda6ba6a8e1212
kunalja <kunaljain@utexas.edu>
2020-04-21 23:46:02 -0500

    added basic search functionality to stocklanding

--- frontend/src/pages/stocklanding.js
+++ frontend/src/pages/stocklanding.js
@@ -7,18 +7,32 @@ import Card from "../components/Card"
 const Stocklanding = ({ data }) => {
   const stocks = data.allMongodbStockInformationInformation.edges
   const [page, setPage] = useState(0)
+  const [displayableItems, setDisplayableItems] = useState(stocks)
   const perPage = 6
-  const numPages = Math.ceil(stocks.length / perPage)
+  const numPages = Math.ceil(displayableItems.length / perPage)
+
+  const searchFunction = e => {
+    if (e.target.value !== "") {
+      setDisplayableItems(stocks.filter(item => item.node.name.toLowerCase().includes(e.target.value.toLowerCase()) || item.node.symbol.toLowerCase().includes(e.target.value.toLowerCase())))
+      setPage(0)
+    } else {
+      setDisplayableItems(stocks);
+    }
+  }
+
   return (
     <>
       <Navbar />
       <div>
-        <h2 class="home" style={{ marginTop: 15, marginLeft: 15, fontWeight: 'bold' }}>
+        <h2 className="home" style={{ marginTop: 15, marginLeft: 15, fontWeight: 'bold' }}>
           Top Stocks: Click on a stock for more info!
         </h2>
       </div>
+      <div className="md-form m-3">
+        <input className="form-control" type="text" onChange={searchFunction} placeholder="Search by name or symbol" aria-label="Search" />
+      </div>
       <div className="card-columns" style={{ paddingLeft: 15, paddingRight: 15 }}>
-        {stocks.slice(page * perPage, (page + 1) * perPage).map(({ node }, i) => {
+        {displayableItems.slice(page * perPage, (page + 1) * perPage).map(({ node }, i) => {
           return (
             <Card
               title={node.symbol}
@@ -35,7 +49,7 @@ const Stocklanding = ({ data }) => {
           )
         })}
       </div>
-      <nav aria-label="Page navigation" className="d-flex justify-content-center mt-3">
+      {numPages > 0 && <nav aria-label="Page navigation" className="d-flex justify-content-center mt-3">
         <ul className="pagination">
           {page !== 0 && <li className="page-item">
             <button
@@ -54,14 +68,15 @@ const Stocklanding = ({ data }) => {
             <button
               className="page-link"
               onClick={
-                () => setPage(Math.min(page + 1, Math.ceil(stocks.length / perPage) - 1))
+                () => setPage(Math.min(page + 1, Math.ceil(displayableItems.length / perPage) - 1))
               }
             >
               Next
             </button>
           </li>}
         </ul>
-      </nav>
+      </nav>}
+      {numPages === 0 && <h1 className="m-3">No results found</h1>}
     </>
   )
 }
